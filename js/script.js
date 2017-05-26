7485bdf506d52cf729b541f251ff7dc38a3eb391
fragileguy <fragileguy7@gmail.com>
2017-05-25 21:38:32 -0400

    changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,69 @@
 var gameWrapper = document.querySelector('#game');
 var form = document.querySelector('#form');
-var rules= document.querySelector('#rules');
+var rules = document.querySelector('#rules');
+
 if (!gameWrapper.classList.contains('hide')) {
     gameWrapper.classList.remove('center');
     gameWrapper.classList.add('hide');
 
 }
-var lose = false;
-var takeMoney = 0;
+
+document.getElementById("butt_rules").onclick = function () {
+    if (rules.classList.contains('hide')) {
+        rules.classList.remove('hide');
+    }
+    if (!form.classList.contains('hide')) {
+        form.classList.add('hide');
+    }
+
+
+}
+document.getElementById("start_game").onclick = function () {
+        
+        if(validate){
+            showGame();
+        game.readTextFile();
+        game.getData();
+        game.startTimer();
+        if (!form.classList.contains('hide')) {
+            form.classList.add('hide');
+        }
+        if (!rules.classList.contains('hide')) {
+            rules.classList.add('hide');
+        }
+    
+        }
+
+
+        var correct = document.getElementsByClassName("answer");
+
+    } //end onclick function start game
+function validate() {
+    var error = document.getElementsByClassName('error');
+    var info = document.getElementsByClassName('info');
+    var firstName = info[0].value;
+    var val=true;
+    
+    var reg = /^[a-zA-Z ]{1,15}$/
+    if (firstName == "" || firstName == null) {
+        error[0].setAttribute('placeholder', 'You Must Enter First Name');
+        val = false;
+        if (reg.test(firstName)) {
+          info[0].setAttribute('placeholder', firstName+'doesnt match requirments, re-enter');
+            val =false;
+        } else {
+            info[0].setAttribute('placeholder', 'Enter Your First Name');
+        }
+    } else {
+        info[0].setAttribute('placeholder', 'Enter Your First Name');
+
+    }
+    
+    if(val){
+        return val;
+    }
+}
+
 
 function showGame() {
 
@@ -29,36 +85,12 @@ function hideGame() {
     if (!gameWrapper.classList.contains('hide')) {
         gameWrapper.classList.add('hide');
         gameWrapper.classList.remove('center');
-    } 
-	if (!rules.classList.contains('hide')) {
-            rules.classList.add('hide');
-        }
-	
+    }
+    if (!rules.classList.contains('hide')) {
+        rules.classList.add('hide');
+    }
+
 } //end hide game
-
-document.getElementById("start_game").onclick = function () {
-        showGame();
-        game.readTextFile();
-        game.getData();
-        game.startTimer();
-        if (!form.classList.contains('hide')) {
-            form.classList.add('hide');
-        }
-        if (lose) {
-            showElement();
-        }
-
-
-
-        var correct = document.getElementsByClassName("answer");
-
-    } //end onclick function start game
-document.getElementById("butt_rules").onclick = function(){
-	 if (rules.classList.contains('hide')) {
-            rules.classList.remove('hide');
-        }
-		
-}
 
 
 window.onload = function () {
@@ -92,7 +124,7 @@ if (typeof (Storage) !== "undefined") {
 }
 //object class start
 var game = {
-    time: 10,
+    time: 240,
     x: "",
     question: "",
     choice: "",
@@ -105,10 +137,13 @@ var game = {
     questionHolder: document.getElementsByClassName("question"),
     questionArray: [],
     moneyHolder: document.getElementsByClassName('moneyTree'),
-    level:8,
+    bank2: document.getElementsByClassName('bank2'),
+    level: 8,
     moneyLevel: 9,
     moneyBank: document.getElementsByClassName('bank'),
     popup: document.getElementById('gameinfoHolder'),
+    completed: false,
+    takeMoney: 0,
 
     /*function using http protocol to get and reads json file raw stored locally, this function is
 	used by the getData function to convert json to readable data by javascript*/
@@ -135,6 +170,9 @@ var game = {
             game.displayQuest(gameQ);
         });
         this.nextStage();
+        if (this.completed) {
+            this.level = 5;
+        }
 
     }, //end showData
 
@@ -206,6 +244,8 @@ var game = {
             console.log("Current level is: " + this.level);
             console.log("^^^^^^^^^^^^^_____^^^^^^^^^^^^");
             //            alert("level right before div change "+this.level)
+            alert("Round two level variable before background class " + this.level)
+
             if (game.moneyHolder[this.level].classList.contains('backGround')) {
                 game.moneyHolder[this.level].classList.remove('backGround');
                 game.moneyHolder[this.level - 1].classList.add('backGround');
@@ -215,15 +255,15 @@ var game = {
                 var bankMoney = confirm('Do You Wish To Bank Money?' + this.userMoney);
 
                 if (bankMoney) {
-                    takeMoney = parseInt(takeMoney);
-                    takeMoney += parseInt(this.userMoney);
+                    this.takeMoney = parseInt(this.takeMoney);
+                    this.takeMoney += parseInt(this.userMoney);
                     //                     if (game.moneyHolder[this.level+1].classList.contains('backGround')) {
-                    game.moneyHolder[6].classList.remove('backGround');
+                    game.moneyHolder[this.level].classList.remove('backGround');
                     game.moneyHolder[8].classList.add('backGround');
                     //  }
                     this.level = 9;
                     this.moneyLevel = 9;
-                    game.moneyBank[0].innerHTML = '$' + takeMoney;
+                    game.moneyBank[0].innerHTML = '$' + this.takeMoney;
                 }
                 //                alert("level of money "+this.level)
                 this.level = (parseInt(this.level) - 1);
@@ -265,7 +305,7 @@ var game = {
 
 
         var x = setInterval(function () {
-			
+
             var minutes = game.time / 60;
             var seconds = game.time % 60;
 
@@ -280,26 +320,24 @@ var game = {
                 game.time--;
 
             }
-           
+
             // If the count down is over, write some text 
             if (game.time == -1) {
-				alert(game.level);
+                var completed = false;
                 clearInterval(x);
-<<<<<<< HEAD
-                this.round = 2;
-				game.level=7;
-=======
-                
-                game.round = 2;
-                
-                game.level=7;
->>>>>>> origin/master
-                game.showRoundTwo();
-                 game.moneyHolder=document.getElementsByClassName('bank2');
-                alert("Bank 2 "+game.moneyHolder.length);
+
+                if (this.completed) {
+                    game.showRoundThree();
+                    game.time = 120;
+                }
+                if (!this.completed) {
+                    game.showRoundTwo();
+                    game.time = 120;
+                    this.completed = true;
+                }
+
                 game.readTextFile();
                 game.getData();
-                game.time=90;
                 game.startTimer();
                 //game.youLose();
             }
@@ -311,13 +349,32 @@ var game = {
     stopTimer: function () {
         clearInterval(x);
     },
-    showRoundTwo:function(){
-        var bank=document.getElementsByClassName('bankWrapper');
-        if(bank[1].classList.contains('hide')){
+    showRoundTwo: function () {
+        var bank = document.getElementsByClassName('bankWrapper');
+        if (bank[1].classList.contains('hide')) {
             bank[1].classList.remove('hide');
             bank[0].classList.add('hide');
+
+            game.round = 2;
+            document.querySelector('.round').innerHTML = game.round;
+
+            game.moneyHolder = document.getElementsByClassName('bank2');
+            game.level = game.moneyHolder.length
+            alert("Bank 2 " + game.level);
+            //game.moneyHolder[game.level].classList.add('backGround');
         }
-        
+
+    },
+    showRoundThree: function () {
+        var bank = document.getElementsByClassName('bankWrapper');
+        if (!bank[1].classList.contains('hide')) {
+            bank[1].classList.add('hide');
+
+            game.round = 3;
+            game.level = 6;
+            document.querySelector('.round').innerHTML = game.round;
+
+        }
     },
     restartGame: function () {
 
@@ -350,29 +407,28 @@ window.onclick = function (event) {
     }
 }
 
-               //////////////////////////// AUDIO SETTINGS/////////////////////////////////////
-		var beep = new Audio;
-		beep.src="Music/gsound.mp3";
-		
-			
-<<<<<<< HEAD
-		// beep.autoplay = true;
-=======
-		//beep.autoplay = true;
->>>>>>> origin/master
-		
-	     function playm(){		
-			beep.autoplay = false;
-			if(beep.paused){
-			
-				beep.play();
-				 document.getElementById("sound").src="Images/volume_on.png"
-			}else {
-				beep.load();
-				 document.getElementById("sound").src="Images/volume_off.png"
-			}
-			  
-		 }
-		 
-		 document.getElementById("sound").onmousedown=playm;
-		 
+//////////////////////////// AUDIO SETTINGS/////////////////////////////////////
+var beep = new Audio;
+beep.src = "Music/gsound.mp3";
+
+
+
+// beep.autoplay = true;
+
+//beep.autoplay = true;
+
+
+function playm() {
+    beep.autoplay = false;
+    if (beep.paused) {
+
+        beep.play();
+        document.getElementById("sound").src = "Images/volume_on.png"
+    } else {
+        beep.load();
+        document.getElementById("sound").src = "Images/volume_off.png"
+    }
+
+}
+
+document.getElementById("sound").onmousedown = playm;
